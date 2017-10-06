5e4a864ffd448cb92a1c0327aef729db7ac38e44
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2017-10-06 16:06:27 +0600

    DataStructure with inner class EvenIterator by iterator

---
+++ oop_concepts/DataStructure.java
@@ -0,0 +1,42 @@
+package oop_concepts;
+
+public class DataStructure {
+    private final static int SIZE=15;
+    private int[] arrayOfInts = new int[SIZE];
+
+    public DataStructure() {
+        for (int i = 0; i <SIZE ; i++) {
+            arrayOfInts[i]=i;
+        }
+    }
+
+    public void printEven(){
+        DataStructureIterator iterator = this.new EvenIterator();
+        while (iterator.hasNext()){
+            System.out.print(iterator.next()+" ");
+        }
+    }
+    interface DataStructureIterator extends java.util.Iterator<Integer>{
+
+    }
+    public class EvenIterator implements  DataStructureIterator{
+        private int nextIndex =0;
+
+        @Override
+        public boolean hasNext() {
+            return (nextIndex<=SIZE-1);
+        }
+
+        @Override
+        public Integer next() {
+            Integer retValue = Integer.valueOf(arrayOfInts[nextIndex]);
+            nextIndex+=2;
+            return  retValue;
+        }
+    }
+
+    public static void main(String[] args) {
+        DataStructure ds = new DataStructure();
+        ds.printEven();
+    }
+}
